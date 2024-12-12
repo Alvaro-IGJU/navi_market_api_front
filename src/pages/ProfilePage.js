cd8ddc5e642f4ac7786d47da5c02c0dc401ed471
salimtamaichi <salim.tamazirt.dev@gmail.com>
2024-12-12 15:26:01 +0100

    implemented react-toastify

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -2,6 +2,8 @@ import React, { useContext, useState, useEffect } from 'react';
 import { AuthContext } from '../contexts/AuthContext';
 import Header from '../components/Header';
 import api from '../api';
+import { toast, ToastContainer } from 'react-toastify';  // Importa toastify
+import 'react-toastify/dist/ReactToastify.css';  // Estilos de react-toastify
 
 const ProfilePage = () => {
   const { user, setUser, renewAccessToken } = useContext(AuthContext);
@@ -20,9 +22,6 @@ const ProfilePage = () => {
     old_password: '',
     new_password: '',
   });
-  const [message, setMessage] = useState('');
-  const [passwordMessage, setPasswordMessage] = useState('');
-  const [preview, setPreview] = useState('');
 
   useEffect(() => {
     if (user) {
@@ -35,7 +34,6 @@ const ProfilePage = () => {
         sector: user.sector || '',
         profile_picture: user.profile_picture || '',
       });
-      setPreview(user.profile_picture || '');
     }
 
     const fetchOptions = async () => {
@@ -78,7 +76,6 @@ const ProfilePage = () => {
       const reader = new FileReader();
       reader.onload = () => {
         setFormData((prevData) => ({ ...prevData, profile_picture: reader.result }));
-        setPreview(reader.result);
       };
       reader.readAsDataURL(file);
     }
@@ -91,7 +88,6 @@ const ProfilePage = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    setMessage(''); // Reset previous message
 
     let accessToken = localStorage.getItem('accessToken');
     if (!accessToken) {
@@ -99,7 +95,7 @@ const ProfilePage = () => {
     }
 
     if (!accessToken) {
-      setMessage('No se pudo autenticar la solicitud.');
+      toast.error('No se pudo autenticar la solicitud.');
       return;
     }
 
@@ -108,21 +104,16 @@ const ProfilePage = () => {
         headers: { Authorization: `Bearer ${accessToken}` },
       });
       setUser(response.data);
-      setMessage('Perfil actualizado exitosamente.');
+      toast.success('Perfil actualizado exitosamente.');
 
-      // Hide message after 3 seconds
-      setTimeout(() => {
-        setMessage('');
-      }, 3000); // Hide after 3 seconds
     } catch (error) {
       console.error('Error al actualizar el perfil:', error);
-      setMessage('Error al actualizar el perfil.');
+      toast.error('Error al actualizar el perfil.');
     }
   };
 
   const handlePasswordSubmit = async (e) => {
     e.preventDefault();
-    setPasswordMessage(''); // Reset password message
 
     let accessToken = localStorage.getItem('accessToken');
     if (!accessToken) {
@@ -130,7 +121,7 @@ const ProfilePage = () => {
     }
 
     if (!accessToken) {
-      setPasswordMessage('No se pudo autenticar la solicitud.');
+      toast.error('No se pudo autenticar la solicitud.');
       return;
     }
 
@@ -138,17 +129,11 @@ const ProfilePage = () => {
       const response = await api.put('/users/change-password/', passwordData, {
         headers: { Authorization: `Bearer ${accessToken}` },
       });
-      setPasswordMessage(response.data.detail || 'Contraseña actualizada exitosamente.');
+      toast.success(response.data.detail || 'Contraseña actualizada exitosamente.');
 
-      // Hide password message after 3 seconds
-      setTimeout(() => {
-        setPasswordMessage('');
-      }, 3000); // Hide after 3 seconds
     } catch (error) {
       console.error('Error al cambiar la contraseña:', error);
-      setPasswordMessage(
-        error.response?.data?.detail || 'Error al cambiar la contraseña.'
-      );
+      toast.error(error.response?.data?.detail || 'Error al cambiar la contraseña.');
     }
   };
 
@@ -157,7 +142,6 @@ const ProfilePage = () => {
       <Header />
       <div className="max-w-4xl mx-auto p-6 bg-gray-800 rounded-lg shadow-lg mt-4 md:mt-4">
         <h1 className="text-3xl font-bold mb-6 text-[#C7AA68]">Actualizar Perfil</h1>
-        {message && <p className="text-lg text-green-500">{message}</p>}
         <form onSubmit={handleSubmit}>
           <div className="mb-4">
             <label className="block font-semibold text-gray-200">Nombre</label>
@@ -207,9 +191,9 @@ const ProfilePage = () => {
               accept="image/*" // Solo permite seleccionar imágenes
               className="w-full px-4 py-2 bg-gray-700 text-gray-100 border border-[#212529] rounded focus:ring focus:ring-[#C7AA68]"
             />
-            {preview && (
+            {formData.profile_picture && (
               <img
-                src={preview}
+                src={formData.profile_picture}
                 alt="Preview"
                 className="mt-4 w-24 h-24 rounded-full border-2 border-[#C7AA68] object-cover"
               />
@@ -225,7 +209,6 @@ const ProfilePage = () => {
         </form>
 
         <h2 className="text-2xl font-bold mt-6 text-[#C7AA68]">Cambiar Contraseña</h2>
-        {passwordMessage && <p className="text-lg text-green-500">{passwordMessage}</p>}
         <form onSubmit={handlePasswordSubmit} className="mt-4">
           <div className="mb-4">
             <label className="block font-semibold text-gray-200">Contraseña Actual</label>
@@ -255,6 +238,8 @@ const ProfilePage = () => {
           </button>
         </form>
       </div>
+
+      <ToastContainer /> {/* Aquí se muestra el ToastContainer */}
     </div>
   );
 };
